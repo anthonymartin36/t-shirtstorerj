e9451900b6c68be35aaeb83ab1a92daa2467a4da
anthonymartin36 <anthony.martin36@gmail.com>
2025-04-08 15:55:11 +1200

    add remote main product list

---
+++ src/Api/api-product.js
@@ -0,0 +1,12 @@
+import request from 'superagent';
+
+const rootUrl = "http://localhost:3000/api/v1";
+
+export async function getAllProducts (){
+  try {
+    const response = await request.get(`${rootUrl}/products`);
+    return response.body;
+  } catch (error) {
+    throw console.error('Error fetching products', error);
+  }
+}
